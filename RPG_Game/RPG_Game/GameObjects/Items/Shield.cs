5913ebbcfe2e581624e26ebbed844a990d53f54f
TeodSt <teod_stoyanova@abv.bg>
2015-12-11 22:47:49 +0200

    Items Shield and Cloack added

---
+++ RPG_Game/RPG_Game/GameObjects/Items/Shield.cs
@@ -0,0 +1,37 @@
+﻿using RPG_Game.GameObjects.Items;
+using RPG_Game.Interfaces;
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using RPG_Game;
+using Microsoft.Xna.Framework;
+
+namespace Items
+{
+    public class Shield : Item, IDefenseRestore
+    {
+        private const int DefaultDefenseRestore = 10;
+
+        public Shield(RPG_Game.Point position)
+            : base(position)
+        {
+            this.DefenseRestore = DefaultDefenseRestore;
+        }
+
+        public int DefenseRestore { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: Defense restored ({1})", this.GetType().Name, this.DefenseRestore);
+        }
+
+        public override void Update(GameTime gameTime)
+        {          
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            //TODO : Add images
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RPG_Game/RPG_Game/Interfaces/IDefenseRestore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG_Game.Interfaces
+{
+    public interface IDefenseRestore
+    {
+        int DefenseRestore { get; set; }
+    }
+}
