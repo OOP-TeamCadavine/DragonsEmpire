64dc8552ec6df15e50be55dadb0e264d926c3564
Plamen Dimitrov <pl.st.dimitrov@gmail.com>
2015-12-14 16:43:09 +0200

    Class Enemy made abstract and added concrete class Dragon.cs.

--- RPG_Game/RPG_Game/GameObjects/Characters/Enemy/Enemy.cs
+++ RPG_Game/RPG_Game/GameObjects/Characters/Enemy/Enemy.cs
@@ -1,35 +1,10 @@
-﻿using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-
-namespace RPG_Game.GameObjects.Characters.Enemy
+﻿namespace RPG_Game.GameObjects.Characters.Enemy
 {
-    public class Enemy : Character
+    public abstract class Enemy : Character
     {
-        public Enemy(Point position, int attackPoints, int defensePoints, int healthPoinst, int damage)
-            : base(position, attackPoints, defensePoints, healthPoinst, damage)
-        {
-                
-        }
-
-        public override void Attack()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Defense()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            throw new NotImplementedException();
-        }
+        public Enemy(Point position, int attackPoints, int defensePoints, int healthPoints, int damage)
+            : base(position, attackPoints, defensePoints, healthPoints, damage)
+        {                
+        }        
     }
 }
