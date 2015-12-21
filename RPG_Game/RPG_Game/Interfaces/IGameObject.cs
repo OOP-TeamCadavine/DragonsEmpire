c49796bfa9f505e3cca81b30fc89554b51728aa5
Plamen Dimitrov <pl.st.dimitrov@gmail.com>
2015-12-21 18:00:33 +0200

    Added collider box

--- RPG_Game/RPG_Game/Interfaces/IGameObject.cs
+++ RPG_Game/RPG_Game/Interfaces/IGameObject.cs
@@ -1,7 +1,11 @@
-﻿namespace RPG_Game.Interfaces
+﻿using Microsoft.Xna.Framework;
+
+namespace RPG_Game.Interfaces
 {
     public interface IGameObject : IUpdateable, IDrawable
     {
         Position Position { get; set; }
+
+        Rectangle ColliderBox { get; set; }
     }
 }
